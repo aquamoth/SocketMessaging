84668fbbcca554d47eef04fd8d91d72c37f206af
Mattias Åslund <mattias.aslund@mantex.se>
2016-05-21 16:11:09 +0200

    Added a FixedSizedQueue that should be easier to use in Connection than the current Queue<>.

---
+++ SocketMessaging.Tests/FixedSizedQueueTests.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketMessaging.Tests
+{
+	[TestClass]
+	public class FixedSizedQueueTests
+	{
+		[TestMethod]
+		public void Queue_inits_as_empty()
+		{
+			var queue = new FixedSizedQueue(1024);
+			Assert.AreEqual(0, queue.Length);
+		}
+
+		[TestMethod]
+		public void Can_write_to_queue_until_full()
+		{
+			var queue = new FixedSizedQueue(1024);
+			queue.Write(new byte[256]);
+			Assert.AreEqual(256, queue.Length);
+			queue.Write(new byte[256]);
+			Assert.AreEqual(512, queue.Length);
+			queue.Write(new byte[256]);
+			Assert.AreEqual(768, queue.Length);
+			queue.Write(new byte[256]);
+			Assert.AreEqual(1024, queue.Length);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(OverflowException))]
+		public void Cant_write_past_queue_full()
+		{
+			var queue = new FixedSizedQueue(1024);
+			queue.Write(new byte[1025]);
+		}
+
+		[TestMethod]
+		public void Can_read_entire_queue_at_once()
+		{
+			var queue = new FixedSizedQueue(1024);
+			var writeBuffer = new byte[256];
+			new Random().NextBytes(writeBuffer);
+			queue.Write(writeBuffer);
+			var readBuffer = queue.Read();
+			CollectionAssert.AreEqual(writeBuffer, readBuffer);
+		}
+
+		[TestMethod]
+		public void Can_read_to_queue_empty()
+		{
+			var queue = new FixedSizedQueue(1024);
+
+			var writeBuffer = new byte[256];
+			new Random().NextBytes(writeBuffer);
+			queue.Write(writeBuffer);
+
+			var readBuffer = queue.Read(200);
+			Assert.AreEqual(200, readBuffer.Length);
+
+			readBuffer = queue.Read(200);
+			Assert.AreEqual(56, readBuffer.Length);
+
+			readBuffer = queue.Read(200);
+			Assert.AreEqual(0, readBuffer.Length);
+		}
+
+		[TestMethod]
+		public void Can_read_and_write_more_than_queue_max_size()
+		{
+			var r = new Random();
+			var queue = new FixedSizedQueue(400);
+
+			var write1 = new byte[256];
+			r.NextBytes(write1);
+			queue.Write(write1);
+			var read1 = queue.Read(200);
+
+			var write2 = new byte[256];
+			r.NextBytes(write2);
+			queue.Write(write2);
+
+			var read2 = queue.Read();
+
+			CollectionAssert.AreEqual(write1.Concat(write2).ToArray(), read1.Concat(read2).ToArray());
+		}
+
+	}
+}
---
+++ SocketMessaging/FixedSizedQueue.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketMessaging
+{
+	public class FixedSizedQueue
+	{
+		public FixedSizedQueue(int queueSize)
+		{
+			_queue = new byte[queueSize + 1]; //Add a token-byte to the queue to discern start-of-read with start-of-write
+		}
+
+		public int Length { get { return (_queue.Length + _writeIndex - _readIndex) % _queue.Length; } }
+
+		public int UnusedQueueLength { get { return (_queue.Length + _readIndex - _writeIndex - 1) % _queue.Length; } }
+
+		public void Write(byte[] buffer)
+		{
+			var bufferIndex = 0;
+			if (_writeIndex >= _readIndex)
+			{
+				bufferIndex = Math.Min(buffer.Length, _queue.Length - _writeIndex - (_readIndex == 0 ? 1 : 0));
+				Array.Copy(buffer, 0, _queue, _writeIndex, bufferIndex);
+				_writeIndex = (_writeIndex + bufferIndex) % _queue.Length;
+			}
+
+			var numberOfBytesLeftToWrite = buffer.Length - bufferIndex;
+			if (numberOfBytesLeftToWrite > 0)
+			{
+				//Here is always _writeIndex < _readIndex
+				//Except when _readIndex == 0, in which case _writeIndex == _queue.Length, but that's ok because freeQueueLength will just be negative instead of 0.
+				if (numberOfBytesLeftToWrite > UnusedQueueLength)
+					throw new OverflowException("Entire buffer does not fit into the queue");
+
+				Array.Copy(buffer, bufferIndex, _queue, _writeIndex, numberOfBytesLeftToWrite);
+				_writeIndex += numberOfBytesLeftToWrite;
+			}
+		}
+
+		internal byte[] Read(int maxReadSize = 0)
+		{
+			var bufferLength = maxReadSize == 0 
+				? this.Length 
+				: Math.Min(this.Length, maxReadSize);
+
+			var buffer = new byte[bufferLength];
+
+			var bufferIndex = 0;
+			if (bufferLength > _queue.Length - _readIndex)
+			{
+				bufferIndex = _queue.Length - _readIndex;
+				Array.Copy(_queue, _readIndex, buffer, 0, bufferIndex);
+				_readIndex = 0;
+			}
+
+			Array.Copy(_queue, _readIndex, buffer, bufferIndex, buffer.Length - bufferIndex);
+			_readIndex += buffer.Length - bufferIndex;
+
+			return buffer;
+		}
+
+
+
+		readonly byte[] _queue;
+		int _writeIndex = 0;
+		int _readIndex = 0;
+	}
+}
